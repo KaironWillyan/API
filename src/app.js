906e86ea01de7dc3f95b113fdd4be14a5bafa7b3
Kairon Willyan <willyankairon1@gmail.com>
2022-08-09 01:06:03 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config')
+
+const app = express();
+const router = express.Router();
+
+//conectar ao banco de dados
+mongoose.connect(config.connectionString)
+
+//carregar as models
+const Product = require('./models/product')
+const Customer = require('./models/customer')
+const Order = require('./models/order')
+
+//carrega as rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route')
+const customerRoute = require('./routes/customer-route')
+const orderRoute = require('./routes/order-route')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+app.use('/customers', customerRoute);
+app.use('/orders', orderRoute);
+
+
+module.exports = app;
---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à API!';
+
+module.exports = {
+    connectionString: 'mongodb+srv://kairon:kairon@cluster.9ij07dy.mongodb.net/test',
+    sendgridKey: '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',
+    containerConnectionString: 'mongodb+srv://kairon:kairon@cluster.9ij07dy.mongodb.net/test'
+}
